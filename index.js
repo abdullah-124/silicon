cf444297f207303abeacc43cb87990ce9880f0b9
unknown <mdabdullahsakib468@gmail.com>
2022-12-23 17:39:47 +0600

    Product added

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const change =(img) =>{
     document.getElementById('banner-img').src = img;
 }
+const productsContainer = document.getElementById('product-container');
+const loadMore = () =>{
+    fetch('/product.json')
+    .then(res => res.json())
+    .then(data => showCard(data))
+}
+const showCard = (data)=>{
+    data.map(card=>{
+        productsContainer.innerHTML+=`
+        <div class="col-lg-3 col-12">
+        <div class="p-card">
+          <img
+            class="img-fluid"
+            src=${card.img}
+            alt=""
+          />
+          <h5>${card.name}</h5>
+          
+          <div class="d-flex justify-content-between align-items-center">
+            <h6>$ ${card.price}</h6>
+            <button class="btn-silicon-outline">Buy</button>
+          </div>
+        </div>
+      </div>
+        `
+    })
+}
+loadMore();
